5b755ba8f27dfb34657e445a06cc6fcc519a3459
nathangero <nathangero@yahoo.com>
2023-11-28 03:59:04 -0800

    implement pwa installation from site button press

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 
 
 ## Credits
+
+[Loading img source inside the dist directory](https://stackoverflow.com/a/47127094)
+
+### Resoureces
+
+[Trigger PWA installation from a button](https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/How_to/Trigger_install_prompt)
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,20 +1,28 @@
 const butInstall = document.getElementById('buttonInstall');
+let installPrompt = null;
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
+    installPrompt = event;
     butInstall.style.visibility = 'visible';
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    console.log("butInstall clicked")
-    butInstall.setAttribute('disabled', true);
-    butInstall.textContent = 'Installed!';
+    if (!installPrompt) {
+        console.log("Couldn't install PWA. Browser may not support it")
+        return;
+    }
+    const result = await installPrompt.prompt();
+    // console.log(`Install prompt was: ${result.outcome}`);
+    
+    if (result.outcome !== 'dismissed') {
+        butInstall.setAttribute('disabled', true);
+        installPrompt = null;
+        butInstall.textContent = 'Installed!';
+    }
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     alert('Successfully installed!')
     console.log('👍', 'appinstalled', event);
